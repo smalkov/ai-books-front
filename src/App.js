07787f7b1219a0cd4fcb70911386878fe377d63a
Малков Сергей Юрьевич <pewpewinmyhead@gmail.com>
2025-04-15 14:02:25 +0300

    deploy to gh_pages

--- src/App.js
+++ src/App.js
@@ -8,21 +8,21 @@ const Books = () => {
       const tg_user = window.Telegram?.WebApp?.initDataUnsafe?.user;
       console.log("initDataUnsafe", window.Telegram?.WebApp?.initDataUnsafe);
       console.log("tg_user", tg_user);
+      if (tg_user?.id) {
+        setIds(tg_user?.id);
+      }
 
       const res = await fetch(`https://humble-yodel-9j6xj4jpjv9cpwj5-8000.app.github.dev/api/profile/${tg_user?.id}`);
       console.log("res", res);
 
       const data = await res.json();
-      if (tg_user?.id) {
-        setIds(tg_user?.id);
-      }
     } catch (e) {
       console.error("We catch error: ", e);
     }
   };
 
   getUser();
-  return <div>Books3 {ids};</div>;
+  return <div>User_id TG: {ids};</div>;
 };
 
 function App() {
