751bb96098046b3bcb0e55ff403805e69f8158a7
Малков Сергей Юрьевич <pewpewinmyhead@gmail.com>
2025-04-15 15:44:22 +0300

    global types for tg

--- src/App.js
+++ src/App.js
@@ -3,23 +3,6 @@ import "./App.css";
 
 const Books = () => {
   // const [ids, setIds] = useState(null);
-  // const getUser = async () => {
-  //   try {
-  //     const tg_user = window.Telegram?.WebApp?.initDataUnsafe?.user;
-  //     console.log("initDataUnsafe", window.Telegram?.WebApp?.initDataUnsafe);
-  //     console.log("tg_user", tg_user);
-  //     if (tg_user?.id) {
-  //       setIds(tg_user?.id);
-  //     }
-
-  //     const res = await fetch(`https://humble-yodel-9j6xj4jpjv9cpwj5-8000.app.github.dev/api/profile/${tg_user?.id}`);
-  //     console.log("res", res);
-
-  //     const data = await res.json();
-  //   } catch (e) {
-  //     console.error("We catch error: ", e);
-  //   }
-  // };
 
   // getUser();
   return <div>User_id TG;</div>;
---
+++ src/global.d.ts
@@ -0,0 +1,54 @@
+interface TelegramTheme {
+  bg_color: string;
+  text_color: string;
+  hint_color: string;
+  link_color: string;
+  button_color: string;
+  button_text_color: string;
+  secondary_bg_color: string;
+  header_bg_color: string;
+  accent_text_color: string;
+  section_bg_color: string;
+  section_header_text_color: string;
+  section_separator_color: string;
+  subtitle_text_color: string;
+  destructive_text_color: string;
+}
+
+interface WebAppUser {
+  id: number;
+  is_bot: boolean;
+  first_name: string;
+  last_name: string;
+  username: string;
+  is_premium: boolean;
+  photo_url: string;
+}
+
+interface WebappData {
+  user: WebAppUser;
+}
+
+interface TelegramHapticFeedback {
+  impactOccurred: (style: "light" | "medium" | "rigid" | "heavy" | "soft") => void;
+  notificationOccurred: (type: "error" | "success" | "warning") => void;
+}
+
+interface TelegramWebapp {
+  initData: string;
+  initDataUnsafe: WebappData;
+  version: string;
+  platform: string;
+  themeParams: TelegramTheme;
+  headerColor: string;
+  backgroundColor: string;
+  expand: () => void;
+  close: () => void;
+  HapticFeedback: TelegramHapticFeedback;
+}
+
+declare interface Window {
+  Telegram?: {
+    WebApp: TelegramWebapp;
+  };
+}
