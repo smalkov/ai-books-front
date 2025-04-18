5b383fe03cc6040d1e7da07a3169b85689f13f9d
Малков Сергей Юрьевич <pewpewinmyhead@gmail.com>
2025-04-18 19:25:42 +0300

    ngrok for tg

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,13 @@
 import axios from "axios";
-const BASE_URL = "http://127.0.0.1:5000/api/libbooks";
-//?page=1&limit=100
+const BASE_URL = "https://565c-94-131-15-230.ngrok-free.app";
 
 export const getBooks = async (page = 1, limit = 50) => {
   return axios
-    .get(`${BASE_URL}?page=${page}&limit=${limit}`)
+    .get(`${BASE_URL}/api/libbooks?page=${page}&limit=${limit}`, {
+      headers: {
+        "ngrok-skip-browser-warning": "true",
+      },
+    })
     .then((res) => {
       console.log("res", res);
       return res?.data;
