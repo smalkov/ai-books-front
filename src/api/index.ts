545290c3064fb1ac19e5d2a54deedd335ba98414
Малков Сергей Юрьевич <pewpewinmyhead@gmail.com>
2025-04-20 19:55:23 +0300

    fx

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,14 @@
 import axios from "axios";
 const BASE_URL = "http://0.0.0.0:8000";
 
-export const getBooks = async (query?: string) => {
+export const getBooks = async (query?: string, limit: number = 50) => {
   return axios
-    .get(`${BASE_URL}/search?query=${query}`)
+    .get(`${BASE_URL}/search`, {
+      params: {
+        query,
+        limit,
+      },
+    })
     .then((res) => {
       console.log("res", res);
       return res?.data;
